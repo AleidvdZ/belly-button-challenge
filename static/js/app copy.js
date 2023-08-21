35417c2ac710e62e488a8187c190d04588b04bee
AleidvdZ <131220504+AleidvdZ@users.noreply.github.com>
2023-08-20 21:10:03 -0500

    0820 work

--- static/js/app copy.js
+++ static/js/app copy.js
@@ -101,4 +101,55 @@ d3.json(url).then(function(data) {
 
 
 
+// from tutoring
+// // Use the D3 library to read in samples.json from the URL.
+// const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+
+// function buildPanel(params) {
+//   console.log(params);
+  
+
+//   //let panel = d3.select('sample-metadata')
+//   // let metapanel = d3.select("#sample-metadata");
+  
+//   // let metainfo = data.metadata.id;
+//   //   console.log(metainfo);
+  
+//     // for (let index = 0; index < data.metadata.length; index++) {
+//     //   dropdown.append("option").text(dropnames[index]).property("value", dropnames[index]);
+
+  
+    
+// }
+
+// function buildCharts(params) {
+//   console.log(params);
+//   let variable1 = d3.select("#selDataset");
+// }
+
+// function optionChanged(params) {
+//   buildPanel(params);
+//   buildCharts(params);
+// }
+
+
+
+
+// // Init to load the dataset, populate the dropdown, take the first entry and pass to panel and chart fuction
+// function init() {
+//   let dropdown = d3.select("#selDataset");
+
+//   d3.json(url).then((data) => {
+//     let dropnames = data.names;
+//     for (let index = 0; index < dropnames.length; index++) {
+//       dropdown.append("option").text(dropnames[index]).property("value", dropnames[index]);
+//     }
+
+//     buildPanel(dropnames[0]);
+//     buildCharts(dropnames[0]);
+//   })
+
+// }
+
+// init();
--- static/js/app.js
+++ static/js/app.js
@@ -2,13 +2,51 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 
-function buildPanel(params) {
-  console.log(params);
+function buildPanel(p) {
+  console.log(p);
 
+  let PANEL = d3.select("#sample-metadata.panel.body");
+  console.log(PANEL);
+
+    d3.json(url).then((data) => { 
+      let patientMetas = data.metadata;
+      let patientInfo = patientMetas.filter(id => id.id == p);
+      console.log(patientInfo)
+ 
+  
+      // for (patientInfo [key, value] of Object.entries(patientInfo)) {
+      //   console.log(`${key}: ${value}`);
+      // }
+     
+      PANEL.append(patientInfo);
+      console.log(PANEL);
+
+  })
 }
 
-function buildCharts(params) {
-  console.log(params);
+
+// functions to build the two charts
+function buildCharts(p) {
+  console.log(p);
+  
+  d3.json(url).then((data) => {
+    let patientIDs = data.samples;
+    let patientID = patientIDs.filter(id => id.id == p);
+    console.log(patientID)
+
+   // Create empty arrays for creating chart
+   otu_id = []
+   otu_labels = []
+   sample_values = []
+
+   
+
+
+
+
+
+
+  })
 }
 
 function optionChanged(params) {
@@ -19,14 +57,15 @@ function optionChanged(params) {
 
 
 
-
+// Init to load the dataset, populate the dropdown, take the first entry and pass to panel and chart fuction
 function init() {
   let dropdown = d3.select("#selDataset");
+  console.log(dropdown);
 
   d3.json(url).then((data) => {
     let dropnames = data.names;
-    for (let index = 0; index < dropnames.length; index++) {
-      dropdown.append("option").text(dropnames[index]).property("value", dropnames[index]);
+    for (let i = 0; i < dropnames.length; i++) {
+      dropdown.append("option").text(dropnames[i]).property("value", dropnames[i]);
     }
 
     buildPanel(dropnames[0]);
